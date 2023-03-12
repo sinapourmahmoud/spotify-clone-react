487cc9fce50b49cfff48b88a88533392338a52ed
sinapourmahmoud <sinapoormahmood@gmail.com>
2023-03-12 22:45:45 +0330

    adding important files

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Searchbar, TopPlay } from "./components";
+import {
+  Discover,
+  TopArtists,
+  AroundYou,
+  ArtistDetails,
+  SongDetails,
+  TopCharts,
+  Search,
+} from "./pages";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex relative">
+      <Searchbar />
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+        <Searchbar />
+        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+          <div className="flex-1 h-fit pb-40">
+            <Routes>
+              <Route path="/" element={<Discover />} />
+              <Route path="/top-artists" element={<TopArtists />} />
+              <Route path="/top-charts" element={<TopCharts />} />
+              <Route path="/around-you" element={<AroundYou />} />
+              <Route path="/artists/:id" element={<ArtistDetails />} />
+              <Route path="/songs/:songid" element={<SongDetails />} />
+              <Route path="/search/:searchTerm" element={<Search />} />
+            </Routes>
+          </div>
+          <div className="xl:sticky relative top-0 h-fit">
+            <TopPlay />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Sidebar from "./Sidebar";
+import Searchbar from "./Searchbar";
+import SongCard from "./SongCard";
+import TopPlay from "./TopPlay";
+import ArtistCard from "./ArtistCard";
+import DetailsHeader from "./DetailsHeader";
+import SongBar from "./SongBar";
+import RelatedSongs from "./RelatedSongs";
+import Loader from "./Loader";
+import Error from "./Error";
+
+export {
+  TopPlay,
+  Sidebar,
+  SongCard,
+  Searchbar,
+  ArtistCard,
+  DetailsHeader,
+  SongBar,
+  RelatedSongs,
+  Loader,
+  Error,
+};
