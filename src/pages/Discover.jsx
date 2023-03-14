3d35fa7361fb8b5f77417a7eeb78df0afa4060d4
sinapourmahmoud <sinapoormahmood@gmail.com>
2023-03-14 10:46:19 +0330

    adding discover pages title

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -3,6 +3,7 @@ import { getDiscovers } from "../apis";
 import { SongCard, Loader } from "./../components";
 import { initialSongs } from "./../store/slices/musicPlayer";
 import { useDispatch, useSelector } from "react-redux";
+import { genres } from "../assets/constants";
 const Discover = () => {
   let { songs } = useSelector((state) => state.musicPlayer);
   let dispatch = useDispatch();
@@ -18,10 +19,22 @@ const Discover = () => {
   }, []);
   if (loading) return <Loader />;
   return (
-    <div className="flex flex-wrap justify-center xl:justify-start gap-3">
-      {songs.map((track) => (
-        <SongCard track={track} key={track.key} />
-      ))}
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center justify-between gap-3 flex-col xl:flex-row">
+        <p className="text-3xl text-white font-bold">Discover</p>
+        <select className="p-1 bg-black text-white cursor-pointer rounded-lg">
+          {genres.map((item) => (
+            <option value={item.value} key={item.value}>
+              {item.title}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="flex flex-wrap justify-center xl:justify-start gap-3">
+        {songs.map((track) => (
+          <SongCard track={track} key={track.key} />
+        ))}
+      </div>
     </div>
   );
 };
